b918de5ad2f3e8462b44b4fdd7f97c61bd150a27
apk <antonini2903@hotmail.com>
2020-04-17 11:24:51 -0500

    Implementing Conway-s Game of Life rules
    - Added buffer grid and attributed
    - Added tests
    - Ammended comments

--- ConwayGameOfLife.py
+++ ConwayGameOfLife.py
@@ -6,6 +6,14 @@ class ConwayGameOfLife:
     """
     Initializes `ConwayGameOfLife` with its grid and its attributes, such as generation count.
 
+    Attributes
+    ----------
+    grid : list[list]
+      The space that hold all the cells.
+    changeBuffer : list[list]
+      Buffer that stores whether a cell changes in the next generation. It is used to indicate if a
+      cell must become dead or alive while transitioning generations.
+
     Parameters
     ----------
     grid : tuple
@@ -24,15 +32,78 @@ class ConwayGameOfLife:
         - If 
     """
     if   isinstance(grid, tuple):
-      self.grid = [[0 for cell in range(grid[0])] for row in range(grid[1])]
+      x, y = grid
+      self.grid = [[0 for cell in range(x)] for row in range(y)]
     elif isinstance(grid, list):
-      # TODO Refactor this code into a function in order to be able to preprocess data into 2D grids
-      # to be able to initialize a `grid`.
-      expectedWidth = len(grid[0])
-      for row in grid:
-        if len(row) != expectedWidth:
-          raise ValueError("Given grid has rows with different lengths.")
-        for cell in row:
-          if cell != 0 and cell != 1:
-            raise ValueError("Only binary values are permitted.")
-      self.grid = grid
+      self.grid = self.initializeGrid(grid)
+    self.width  = len(self.grid[0])
+    self.height = len(self.grid)
+    
+    # TODO change grid to have surrounding columns and rows, in order to ease implementation of
+    # self.getNeighbors()
+    #self.grid = [[None for x in range(self.width +2)] for y in range(self.height +2)]
+
+    self.changeBuffer = [[0 for cell in range(self.width)] for row in range(self.height)]
+    self.nextGenerationBuffer()
+
+  @staticmethod
+  def initializeGrid(grid):
+    """
+    Checks if given grid is valid.
+    """
+    expectedWidth = len(grid[0])
+    for row in grid:
+      if len(row) != expectedWidth:
+        raise ValueError("Given grid has rows with different lengths.")
+      for cell in row:
+        if cell != 0 and cell != 1:
+          raise ValueError("Only binary values are permitted.")
+    return grid
+
+  def getNeighbors(self, position) -> list:
+    """
+    Returns a list with all the neighbors that a given cell has.
+    """
+    x, y = position
+    grid = [[None for x in range(self.width +2)] for y in range(self.height +2)]
+    for row in range(1, self.height +1):    # Index from second row and column to second to last row
+      for cell in range(1, self.width +1):  # and column
+        grid[row][cell] = self.grid[row -1][cell -1]  # indexed for self.grid must be offset.
+
+    neighbors = [x for y in grid for x in y]  # Traverse `grid` and list all the
+    # neighboring cells. `None` values are discarded.
+    #if len(neighbors) > 8: raise BaseException()  # It will actually happen as `neighbors` as it
+    del neighbors[4]  # Delete the cell in the given position as itself can't be a neighbor.
+    neighbors = [x for y in neighbors if x != None]  # Traverse `grid` and list all the
+    return neighbors
+
+  def willChange(self, position):
+    """
+    If this cell 
+    """
+    def countAlive():
+      return sum(self.getNeighbors(position))
+    x, y = position
+    isCellAlive = self.grid[y][x]
+    aliveNeighbors = countAlive()
+    if isCellAlive:
+      return aliveNeighbors not in range(2, 4)  # won't change if given cell has either 2 or 3
+      # alive neighbor cells, otherwise, flag it for killing.
+    else:
+      return aliveNeighbors == 3  # Given cell will become alive if it has exactly 3 neighbors.
+      
+  def nextGenerationBuffer(self):
+    for row in range(self.height):
+      for cell in range(self.width):
+        self.changeBuffer[row][cell] = self.willChange(position)
+  
+  def nextGeneration(self):
+    for row in range(self.height):
+      for cell in range(self.width):
+        if self.changeBuffer[row][cell]:
+          self.grid[row][cell] = int(not self.grid[row][cell])
+
+  def getGrid(self):
+    return self.grid
+
+    
--- ConwayGameOfLife_tests.py
+++ ConwayGameOfLife_tests.py
@@ -3,14 +3,14 @@ from random import randint
 import unittest
 import ConwayGameOfLife
 
-class ConwayGameOfLifeBaseTest(unittest.TestCase):
+class BaseTest(unittest.TestCase):
   def setUp(self):
     pass
 
   def tearDown(self):
     pass
 
-class CreatingGame(ConwayGameOfLifeBaseTest):
+class CreatingGame(BaseTest):
   def setUp(self):
     pass
 
@@ -51,6 +51,110 @@ class CreatingGame(ConwayGameOfLifeBaseTest):
   def tearDown(self):
     pass
 
+class NeighborDetection(BaseTest):
+  def createSmallGrid(self):
+    grid_initial = [
+      [0,1,0],
+      [1,1,1],
+      [0,1,0],
+    ]
+    self.game = ConwayGameOfLife.ConwayGameOfLife(grid_initial)
+  
+  def createBigGrid(self):
+    grid_initial = [
+      [0,0,1,0,0],
+      [0,1,0,1,0],
+      [1,0,0,0,1],
+      [0,1,0,1,0],
+      [0,0,1,0,0],
+    ]
+    self.game = ConwayGameOfLife.ConwayGameOfLife(grid_initial)
+
+  def test_neighborsAt2_2_BigGrid(self):
+    self.createSmallGrid()
+    position          = (2,2)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [1,0,1,0,0,1,0,1]
+  
+  def test_neighborsAt1_1_BigGrid(self):
+    self.createSmallGrid()
+    position          = (1,1)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [0,0,1,0,0,1,0,0]
+  
+  def test_neighborsAt3_4_BigGrid(self):
+    self.createSmallGrid()
+    position          = (3,4)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [0,0,1,0,0]
+
+  def test_neighborsAt1_1_smallGrid(self):
+    self.createSmallGrid()
+    position          = (1,1)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [0,1,0,1,1,0,1,0]
+  
+  def test_neighborsAt0_0_smallGrid(self):
+    self.createSmallGrid()
+    position          = (0,0)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [1,1,1]
+  
+  def test_neighborsAt2_2_smallGrid(self):
+    self.createSmallGrid()
+    position          = (2,2)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [1,1,1]
+  
+  def test_neighborsAt1_2_smallGrid(self):
+    self.createSmallGrid()
+    position          = (1,2)
+    neighbors         = self.game.getNeighbors(position)
+    expectedNeighbors = [1,1,1,0,0]
+  
+class TestRules(BaseTest):
+  def grid1(self):
+    grid_initial = [
+      [0,1,0],
+      [0,1,0],
+      [0,1,0],
+    ]
+    self.game = ConwayGameOfLife.ConwayGameOfLife(grid_initial)
+
+  def test_OneCellDies(self):
+    grid_initial = [
+      [0,0,0],
+      [0,1,0],
+      [0,0,0],
+    ]
+    self.game = ConwayGameOfLife.ConwayGameOfLife(grid_initial)
+    grid_expected = [[0 for cell in range(3)] for row in range(3)]
+    self.game.nextGeneration()
+    self.assertEqual(self.game.grid, grid_expected)
+
+  def test_threeCellsChange(self):
+    self.grid1()
+    grid_expected = [
+      [0,0,0], 
+      [1,1,1], 
+      [0,0,0], 
+    ]
+    self.game.nextGeneration()
+    self.assertEqual(self.game.grid, grid_expected)
+
+  def test_TwoCellsDie(self):
+    grid_initial = [
+      [0,0,0],
+      [0,1,0],
+      [0,1,0],
+    ]
+    self.game = ConwayGameOfLife.ConwayGameOfLife(grid_initial)
+    grid_expected = [[0 for cell in range(3)] for row in range(3)]
+    self.game.nextGeneration()
+    self.assertEqual(self.game.grid, grid_expected)
+
+class ExpectedErrors(BaseTest):
+  pass
 
 if __name__ == '__main__':
   unittest.main()
