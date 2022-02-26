58301a4857ada2132dfd7acc6e64e649052907f2
71walceli <antonini2903@hotmail.com>
2022-02-26 00:21:06 -0500

    Stiles refactored
    - Added `Tile` borders

--- conway-game-of-life-react/src/App.js
+++ conway-game-of-life-react/src/App.js
@@ -1,12 +1,13 @@
 //import logo from './logo.svg';
 import Game from './Game.js'
 import './App.css';
+import Arrows from '@rsuite/icons';
 
 function App() {
   const grid = {}
   grid.width =  5
   grid.height = 5
-  grid.tiles = Array(grid.width*grid.height).fill().map( () => Math.round(Math.random(0,1)) )
+  //grid.tiles = Array(grid.width*grid.height).fill().map( () => Math.round(Math.random(0,1)) )
 
   return (
     <Game grid={grid} />
