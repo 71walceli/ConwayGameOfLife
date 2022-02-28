c8f856d449664ea9ac19cecd982ee1122dcc3cc6
71walceli <antonini2903@hotmail.com>
2022-02-28 16:28:01 -0500

    Added grid main state and data

--- conway-game-of-life-react/src/App.js
+++ conway-game-of-life-react/src/App.js
@@ -1,8 +1,24 @@
 //import logo from './logo.svg';
 import Game from './Game.js'
 import { ArrowLeft, ArrowRight } from '@rsuite/icons';
+import Grid from './Grid.js'
+import { useState } from 'react';
+
 
 function App() {
+  // TODO Just xample data. Should be null in the beginning. and set un through menu.
+  const gridData = {}
+  gridData.width =  5
+  gridData.height = 5
+  gridData.tiles = [
+    [0,0,0,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,0,0,0],
+  ]
+  // TODO Move to actual game as it should be more eresponsible for the grid.
+  const [grid, setGrid] = useState(new Grid(gridData));
   const rockerButtonStyle = {
     color: 'cyan',
     backgroundColor: "black",
@@ -11,7 +27,7 @@ function App() {
 
   return (
     <div>
-    <Game grid={grid} />
+      <Game grid={grid}  />
       <div className='RockerButtonContainer'>
         <button style={rockerButtonStyle}>
           <ArrowLeft />
