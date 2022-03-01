995f81643884d02d1386a54db63227773a2b8428
71walceli <antonini2903@hotmail.com>
2022-02-28 23:47:09 -0500

    Code refactoring and Organization

--- conway-game-of-life-react/src/App.js
+++ conway-game-of-life-react/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Game from './Game.js'
 import { ArrowLeft, ArrowRight } from '@rsuite/icons';
 import Grid from './Grid.js'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
-  // TODO Just xample data. Should be null in the beginning. and set un through menu.
+  const [game, setGame] = useState()
+  // TODO To be refactored
+  const rockerButtonStyle = {
+    color: 'cyan',
+    backgroundColor: "black",
+    fontSize: "1.5em",
+  };
+  // TODO Just sample data. Should be null in the beginning. and set un through menu.
   const gridData = {}
   gridData.width =  5
   gridData.height = 5
@@ -18,14 +25,7 @@ function App() {
     [0,0,1,0,0],
     [0,0,0,0,0],
   ]
-  // TODO Move to actual game as it should be more eresponsible for the grid.
-  const [grid, setGrid] = useState(new Grid(gridData));
-  const rockerButtonStyle = {
-    color: 'cyan',
-    backgroundColor: "black",
-    fontSize: "1.5em",
-  };
-  
+
   return (
     <div>
       <Game grid={gridData} parentBinder={setGame} />
