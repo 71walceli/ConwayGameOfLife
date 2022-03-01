729aac37a63917f4b10ffdd0cd22401983e4a0a3
71walceli <antonini2903@hotmail.com>
2022-02-28 23:17:07 -0500

    Code refactoring and Organization
    - converted to class
    - it's now in charge of it's own Grid instance.
    - Added dependencies
    - Will le bound to it's parent through `parentBinder`

--- conway-game-of-life-react/src/Game.js
+++ conway-game-of-life-react/src/Game.js
@@ -1,24 +1,50 @@
+import Grid from './Grid.js';
+import React from 'react';
 import Tile from './Tile.js'
 
-const Game = props => {
-  // TODO App must manage it's own style
-  const grid = props.grid;
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      grid: new Grid(props.grid)
+    }
+  }
   
-  return(
-    <table className="grid">
-      <tbody> 
-        {grid.tiles.map((row, y) =>
-          <tr key={`Row ${y}`}> 
-            {row.map((cell, x) => 
-              <td key={`Tile ${x}, ${y}`}>
-                <Tile state={cell} />
-              </td>
-            )}
-          </tr>
-        )}
-      </tbody>
-    </table>
-  )
+  componentDidMount() {
+    this.props.parentBinder(this)
+  }
+
+  nextStep() {
+    this.setState({
+      grid: this.state.grid.nextStep()
+    })
+  }
+
+  handleTileClick(x,y) {
+    const grid = this.state.grid.clone()
+    grid.tiles[y][x] = grid.tiles[y][x] ? 0 : 1
+    this.setState({
+      grid: grid
+    })
+  }
+
+  render() {
+    return (
+      <table className="grid">
+        <tbody> 
+          {this.state.grid.tiles.map((row, y) =>
+            <tr key={`Row ${y}`}> 
+              {row.map((cell, x) => 
+                <td key={`Tile ${x}, ${y}`}>
+                  <Tile state={cell} game={this} onClick={ () => this.handleTileClick(x,y) } />
+                </td>
+              )}
+            </tr>
+          )}
+        </tbody>
+      </table>
+    )
+  }
 }
 
 export default Game
