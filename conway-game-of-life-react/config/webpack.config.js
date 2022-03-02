6af4440bf9c6f34e88704a9912b2a92a98ee0648
71walceli <antonini2903@hotmail.com>
2022-03-02 11:43:01 -0500

    Added additional SASS configs

--- conway-game-of-life-react/config/webpack.config.js
+++ conway-game-of-life-react/config/webpack.config.js
@@ -541,6 +541,19 @@ module.exports = function (webpackEnv) {
                 'sass-loader'
               ),
             },
+            {
+              test: sassModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 3,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                  modules: {
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
+                },
+                'sass-loader'
+              ),
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
