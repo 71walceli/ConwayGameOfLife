d06da8741bdc98925ce4e9c529ec76345ea20b1e
71walceli <antonini2903@hotmail.com>
2022-03-20 18:52:28 -0500

    Code refactoring and organization

--- api/index.js
+++ api/index.js
@@ -30,43 +30,137 @@ app.use(cors())
 app.use(bodyParser.json());
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('API backend is listening!')
+app.get('/', (request, response) => {
+  response.send({message: 'API backend is listening!'})
 })
 
-app.get('/users', (req, res, next) => {
-  connection.query('SELECT id, name, username FROM user', (error, results, fields) => {
-    if (error) {
-      next(error)
-      return
-    }
-    const users = []
-    results.map(row => {
-      users.push({...row})
+
+//#region /users
+  app.get('/users', (request, response, next) => {
+    connection.query('SELECT id, name, username FROM user', (error, results, fields) => {
+      if (error) {
+        next(error)
+        return
+      }
+      const records = results.map(row => ({...row}) )
+      response.json(records)
     })
     res.json(users)
   })
-})
-
-app.post('/users', (req, res, next) => {
-  const newUser = req.body
-  const sqlError = {}
-  const sql = `INSERT INTO user (name, username, password_md5) 
-    VALUES (
-      ${connection.escape(newUser.name)},
-      ${connection.escape(newUser.username)},
-      unhex(sha2(${connection.escape(newUser.password)},512))
-    )`
-  connection.query(sql, (error, results, fields) => {
+  app.post('/users', (request, response, next) => {
+    const newUser = request.body
+    const sqlErrorData = {}
+    const sql = `INSERT INTO user (name, username, password_md5) 
+      VALUES (
+        ${connection.escape(newUser.name)},
+        ${connection.escape(newUser.username)},
+        unhex(sha2(${connection.escape(newUser.password)},512))
+      )`
+    connection.query(sql, (error, results, fields) => {
+        if (error) {
+          sqlErrorData.code =  error.code
+          sqlErrorData.errno =  error.errno
+          sqlErrorData.sqlMessage =  error.sqlMessage
+          sqlErrorData.message =  error.sqlMessage // XXX
+          logError(request, {sqlErrorData: sqlErrorData});
+          response.status(400)
+          response.json(sqlErrorData)
+          if (sqlErrorData.code === "ER_DUP_ENTRY") {
+            next(error)
+            return
+          } else {
+            throw error
+          }
+        }
+        response.json({})
+      }
+    );
+  })
+  app.post('/users/login', async (request, response, next) => {
+    const credentials = request.body
+    const sql = `SELECT id, name, username FROM user
+      WHERE
+        username = ${connection.escape(credentials.username)} AND
+        password_md5 = unhex(sha2(${connection.escape(credentials.password)}, 512))`
+    /*
+    results = await sqlQuery(connection, sql).on("results", results => {
+      console.log(results)
+      return results
+    })
+    //console.log(results)
+    //console.log(results.data)
+    response.json(results.data)
+    */
+    connection.query(sql, (error, results, fields) => {
       if (error) {
-        sqlError.code =  error.code
-        sqlError.errno =  error.errno
-        sqlError.sqlMessage =  error.sqlMessage
-        logError(req, {sqlError: sqlError});
-        res.status(400)
-        res.json(sqlError)
-        if (sqlError.code === "ER_DUP_ENTRY") {
-          next(error)
+        const sqlErrorData = handleSqlError();
+        logError(request, { sqlErrorData: sqlErrorData });
+        response.status(400)
+        response.json(sqlErrorData)
+        return
+      }
+      const records = results.map( row => ({...row}) )
+      if (records.length === 0) {
+        response.status(400)
+      }
+      response.json(records)
+    })
+  })
+  app.put('/users', (request, respoense, next) => {
+    const newUser = request.body
+    const sqlErrorData = {}
+    const sql = `UPDATE user 
+      SET 
+        name = ${connection.escape(newUser.name)},
+        username = ${connection.escape(newUser.username)},
+        password_md5 = unhex(sha2(${connection.escape(newUser.password)},512))
+      WHERE id = ${connection.escape(newUser.id)}`
+    connection.query(sql, (error, results, fields) => {
+        if (error) {
+          sqlErrorData.code =  error.code
+          sqlErrorData.errno =  error.errno
+          sqlErrorData.sqlMessage =  error.sqlMessage
+          sqlErrorData.message =  error.sqlMessage // XXX
+          respoense.status(400)
+          respoense.json(sqlErrorData)
+          if (sqlErrorData.code === "ER_DUP_ENTRY") {
+            next(error)
+            return
+          } else {
+            throw error
+          } 
+        } else if (results.affectedRows === 0) {
+          respoense.status(400)
+          respoense.json({message: "No such record."})
+          next(new Error("No such record."))
+          return
+        }
+        respoense.json({})
+      }
+    );
+  })
+  app.delete('/users', (request, respoense, next) => {
+    const id = request.body.id
+    const sqlErrorData = {}
+    const sql = `DELETE FROM user WHERE id = ${connection.escape(id)}`
+    connection.query(sql, (error, results, fields) => {
+        if (error) {
+          sqlErrorData.code =  error.code
+          sqlErrorData.errno =  error.errno
+          sqlErrorData.sqlMessage =  error.sqlMessage
+          sqlErrorData.message =  error.sqlMessage // XXX
+          respoense.status(400)
+          respoense.json(sqlErrorData)
+          if (sqlErrorData.code === "ER_DUP_ENTRY") {
+            next(error)
+            return
+          } else {
+            throw error
+          } 
+        } else if (results.affectedRows === 0) {
+          respoense.status(400)
+          respoense.json({message: "No such record."})
+          next(new Error("No such record."))
           return
         } else {
           throw error
@@ -77,37 +171,8 @@ app.post('/users', (req, res, next) => {
   );
 })
 
-app.put('/users', (req, res, next) => {
-  const newUser = req.body
-  const sqlError = {}
-  const sql = `UPDATE user 
-    SET 
-      name = ${connection.escape(newUser.name)},
-      username = ${connection.escape(newUser.username)},
-      password_md5 = unhex(sha2(${connection.escape(newUser.password)},512))
-    WHERE id = ${connection.escape(newUser.id)}`
-  connection.query(sql, (error, results, fields) => {
-      if (error) {
-        sqlError.code =  error.code
-        sqlError.errno =  error.errno
-        sqlError.sqlMessage =  error.sqlMessage
-        logError(req, {sqlError: sqlError});
-        res.status(400)
-        res.json(sqlError)
-        if (sqlError.code === "ER_DUP_ENTRY") {
-          next(error)
-          return
-        } else {
-          throw error
-        }
-      }
-      res.json({})
-    }
-  );
-})
-
-app.use((err, req, res, next) => {
-  logError(req)
+app.use((error, request, response, next) => {
+  logError(request)
   //res.status(500).send("Oops, something went wrong.")
 })
 
@@ -124,3 +189,14 @@ function logError(req, extraData) {
     ...extraData,
   });
 }
+
+function handleSqlError(error) {
+  const sqlErrorData = {};
+  sqlErrorData.code = error.code;
+  sqlErrorData.errno = error.errno;
+  sqlErrorData.sqlMessage = error.sqlMessage;
+  sqlErrorData.message = error.sqlMessage; // XXX
+  next(error);
+  return sqlErrorData;
+}
+
